5749e90cc728e6caa28074fd2dde091d46a51b8e
Charlie Greco <cgreco812@gmail.com>
2025-03-27 11:00:00 -0400

    Added basic search

--- .idea/index.js
+++ .idea/index.js
@@ -1,1 +1,32 @@
-console.log("Hello from Node.js!");
+url = "https://gutendex.com/books/?search="
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+async function promtUser(promt){
+    return new Promise(resolve => {
+        rl.question(promt, answer => {
+            resolve(answer);
+        })
+    })
+}
+
+async function getSearchTerm() {
+    const term = await promtUser("What would you like to search for? ");
+
+    const response = await fetch(url + "" + term)
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+    const json = await response.json();
+    print(json)
+}
+
+getSearchTerm()
+
+
